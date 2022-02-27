3dd38557ebf5a2d00c14cee6aeec3cb943d44559
yashjain02 <45165110+yashjain02@users.noreply.github.com>
2022-02-27 14:28:58 +0100

    Update SixthQuery.py

--- SixthQuery.py
+++ SixthQuery.py
@@ -2,7 +2,7 @@
 import pymongo as pymongo
 def connectdb():
     client = pymongo.MongoClient(
-        "mongodb+srv://yash_kumar:yashjain@cluster0.oaexh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        "mongodb+srv://yash_kumar:<password>@cluster0.oaexh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     db = client.test
     col = db['countries']
     cursor=col.find({'name': {'$regex': 'u','$options':'i'}, 'population':{'$gt':100000}},{'name':1, '_id':0, 'population':1})
