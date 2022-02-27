7db326ef496821a38fd8e18964b8bb631aae911c
yashjain02 <45165110+yashjain02@users.noreply.github.com>
2022-02-27 14:26:32 +0100

    Update FifthQuery.py

--- FifthQuery.py
+++ FifthQuery.py
@@ -1,8 +1,8 @@
-#Get all teh countries order by number of people, first less populated and last highly populated
+#Get all teh countries order by number of people, first less populated and last highly populated, I have removed my password for security purpose
 import pymongo as pymongo
 def connectdb():
     client = pymongo.MongoClient(
-        "mongodb+srv://yash_kumar:yashjain@cluster0.oaexh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        "mongodb+srv://yash_kumar:<password>@cluster0.oaexh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     db = client.test
     col = db['countries']
     cursor=col.find({},{'_id':0, 'name':1, 'population':1}).sort('population')
