69a48ca01cf354efbfea00b17dd502e142a8cf5a
yashjain02 <45165110+yashjain02@users.noreply.github.com>
2022-02-27 14:29:20 +0100

    Update ThirdQuery.py

--- ThirdQuery.py
+++ ThirdQuery.py
@@ -2,7 +2,7 @@
 import pymongo as pymongo
 def connectdb():
     client = pymongo.MongoClient(
-        "mongodb+srv://yash_kumar:yashjain@cluster0.oaexh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        "mongodb+srv://yash_kumar:<password>@cluster0.oaexh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     db = client.test
     col = db['continents']
     cursor=col.find({},{'name':1, 'countries':{'$slice':4}}).sort('countries')
