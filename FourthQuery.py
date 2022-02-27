f79e80c1db4004e2ea096d9df5805c4fa4ff4a5d
yashjain02 <45165110+yashjain02@users.noreply.github.com>
2022-02-27 16:44:53 +0100

    Update FourthQuery.py

--- FourthQuery.py
+++ FourthQuery.py
@@ -6,10 +6,12 @@ def connectdb():
     db = client.test
     col = db['countries']
     col.updateMany({}, {'$set': {"population": 'null'}})
-    col.update_one({'name':'USA'},{'$set':{'population': 332915073}})
+    col.update_one({'name':'USA'},{'$set':{'population': 332403650}})
     col.update_one({'name': 'Greece'}, {'$set': {'population': 10720000}})
-    col.update_one({'name': 'germany'}, {'$set': {'population': 832400000}})
-    col.update_one({'name': 'South africa'}, {'$set': {'population': 593100000}})
-    col.update_one({'name': 'India'}, {'$set': {'population': 14000000000}})
+    col.update_one({'name': 'germany'}, {'$set': {'population':83240000}})
+    col.update_one({'name': 'South africa'}, {'$set': {'population': 59310000}})
+    col.update_one({'name': 'India'}, {'$set': {'population': 140000000}})
+    col.update_one({'name': 'France'}, {'$set': {'population': 65800000}})
+    col.update_one({'name': 'Italy'}, {'$set': {'population': 59550000}})
 if __name__ == '__main__':
     connectdb()
